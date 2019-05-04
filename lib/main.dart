627f3cdb6444df67a49578885c739753fb92adcf
Nikhil <r.nikhil1999@gmail.com>
2019-05-04 12:38:47 +0530

    Flutter_Whatsapp layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
-    void main() => runApp(MaterialApp(
-      title: "Flutter",
-        home: Scaffold(
+    void main() {
+      runApp(MaterialApp(title: "my new app",home: HomeWidget()));
+
+}
+      class HomeWidget extends StatelessWidget{
+        @override
+      Widget build(BuildContext context){
+          return Scaffold(
           drawer: Drawer(),
           appBar: AppBar(
-            title: Text("My header"),
-          ),
-          body: Center(
-            child: Text("Welcome to Flutter",style: TextStyle(fontSize: 20,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),)),
-
+      title: Text("My header"),
+      ),
+      body:ListView.builder(itemCount:20,itemBuilder:(context,index){
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          CircleAvatar(backgroundColor: Colors.blue,
+          child: Text("$index"),
         ),
-      ));
+        Text("User Name"),
+        Text("2:00")
+
+
+
+        ],);
+      })
+          );
+      }
+    }
+
